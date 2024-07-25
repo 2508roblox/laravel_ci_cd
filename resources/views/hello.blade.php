538aa0f217862c6d06e75e7e5361aef310f0641d
2508roblox <trangiangzxc@gmail.com>
2024-07-25 17:23:30 +0700

    update view

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -16,6 +16,6 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-       Hello
+       Github Actions CI/CD for Laravel Auto Deploy with Deployer.sh update ssh private key
     </body>
 </html>
